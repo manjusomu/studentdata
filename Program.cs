d2dff66038517b4753e8897e2584b3ed51ec64eb
manjusomu <manjusomu2001@gmail.com>
2023-12-16 20:19:53 +0530

    changes

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+     
+     
+            // Specify the path to your text file
+            string filePath = @"C:\Users\LENOVO\Documents\new\student.txt";
+          //  File.Create(filePath);
+          if(File.Exists(filePath))
+        {
+            string[] lines= File.ReadAllLines(filePath);
+            foreach(var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine("invalid file path");
+
+        }
+          
+    }
+}
